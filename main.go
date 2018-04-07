4a1beada2933c1a761448e039c1d364a9adcfcae
Luuk Verweij <luuk_verweij@msn.com>
2018-04-07 15:21:47 +0200

    Initial commit; go + selenium + geckodriver + firefox working

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/tebeka/selenium"
+)
+
+const (
+	geckoDriverPath = "/usr/bin/geckodriver"
+	port            = 4444
+)
+
+func main() {
+	caps := selenium.Capabilities{"browserName": "firefox"}
+	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d", port))
+	if err != nil {
+		panic(err)
+	}
+	defer wd.Quit()
+
+	// Navigate to the simple playground interface.
+	if err := wd.Get("http://play.golang.org/?simple=1"); err != nil {
+		panic(err)
+	}
+
+	// Get a reference to the text box containing code.
+	elem, err := wd.FindElement(selenium.ByCSSSelector, "#code")
+	if err != nil {
+		panic(err)
+	}
+	// Remove the boilerplate code already in the text box.
+	if err := elem.Clear(); err != nil {
+		panic(err)
+	}
+
+	// Enter some new code in text box.
+	err = elem.SendKeys(`
+		package main
+		import "fmt"
+		func main() {
+			fmt.Println("Hello WebDriver!\n")
+		}
+	`)
+	if err != nil {
+		panic(err)
+	}
+
+	// Click the run button.
+	btn, err := wd.FindElement(selenium.ByCSSSelector, "#run")
+	if err != nil {
+		panic(err)
+	}
+	if err := btn.Click(); err != nil {
+		panic(err)
+	}
+
+	// Wait for the program to finish running and get the output.
+	outputDiv, err := wd.FindElement(selenium.ByCSSSelector, "#output")
+	if err != nil {
+		panic(err)
+	}
+
+	var output string
+	for {
+		output, err = outputDiv.Text()
+		if err != nil {
+			panic(err)
+		}
+		if output != "Waiting for remote server..." {
+			break
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+
+	fmt.Printf("%s", strings.Replace(output, "\n\n", "\n", -1))
+
+	time.Sleep(1 * time.Second)
+
+}
